05f367750ad5e5ad809672e4a6653fcfd30d5136
Francel <francel.webdev@gmail.com>
2019-04-20 15:40:45 +0100

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,12 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+
+    # Mon code
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # Mon code
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
