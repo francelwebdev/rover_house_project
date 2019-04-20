8c0a26871d56e1d508e7e279f4526bbd5bb5a9f2
Francel <francel.webdev@gmail.com>
2019-04-20 15:31:01 +0100

    ok

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
-    mount_uploader :name, PhotosUploader
-    validates :name, presece: true
+    # mount_uploader :name, PhotosUploader
+    # validates :name, presece: true
 
   # belongs_to :property
 end
